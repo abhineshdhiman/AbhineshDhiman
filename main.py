8c12e87fcf6bb418d198bfe667aa25b55fc01143
Abhinesh Dhiman <>
2021-04-15 03:38:56 +0000

    #BandNameGenerator

---
+++ main.py
@@ -0,0 +1,47 @@
+# print('Day 1 - Python Print Function')
+# print("The function is declared like this")
+# print("print('what to print')")
+
+# print("Hello world!\nHello world!")
+
+#String Concatination
+# print("hello" + " Abhinesh")
+
+# print("Hello " + input("what is Your name?\n"))
+
+
+#  Printing Characters of name
+
+# print(len(input("What is Your Name\n").replace(" ","")))
+
+
+#variables in Pyton
+
+# name = input("What is your name\n")
+# print("Your Name is " + name)
+
+# name = "New Name"
+# print("Your Name is " + name)
+
+
+#Swap the value from different variables
+
+# a = input("Enter the value for a\n")
+# b = input("Enter the value for b\n")
+# print("value of a is = " + a + " value of b is =" +b )
+# c = a
+# a = b
+# b = c
+# print("value of a after swap is = " + a + " value of b after swap is =" +b )
+
+
+#Variable naming 
+
+
+
+#Code For Band Name Generator
+
+print("Welcome to brand name generator\n")
+cityName = input("Which City You Grew Up  In\n")
+petName = input("what is your Pet Name\n")
+print("Your Band Name May be Like " + cityName + " " + petName)
